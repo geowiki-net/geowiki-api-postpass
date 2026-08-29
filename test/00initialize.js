e87cbb906de61a266a7c92485aba8c0ec090088d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-08-29 20:10:19 +0200

    Test 00initialize: check server availability

--- test/00initialize.js
+++ test/00initialize.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+
 describe('DBTypePostpass', function () {
   let db
   let geowiki
@@ -5,5 +7,18 @@ describe('DBTypePostpass', function () {
   it('initialize', function () {
     db = require('./src/dbTypePostpass')
     geowiki = require('./src/geowikiAPI')
+  })
+
+  it('test server availability', function (done) {
+    db.execute({subRequests:[{query: 'SELECT 1'}]},
+      (err, result) => {
+        if (err) {
+          assert.fail('Server problem: ' + err.cause)
+          return done()
+        }
+
+        done()
+      }
+    )
   })
 })
